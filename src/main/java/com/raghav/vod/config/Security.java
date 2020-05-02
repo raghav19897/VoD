7611c9be17c664bf5122bcf4efad0957187a6033
raghav <raghav19897@gmail.com>
2020-05-03 03:53:35 +0530

    added spring security basic

---
+++ src/main/java/com/raghav/vod/config/Security.java
@@ -0,0 +1,25 @@
+package com.raghav.vod.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class Security extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("raghav")
+        .password("raghav")
+        .roles("ADMIN");
+  }
+
+  @Bean
+  public PasswordEncoder getPasswordEncoder(){
+    return NoOpPasswordEncoder.getInstance();
+  }
+}
